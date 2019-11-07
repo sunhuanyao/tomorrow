df1d1cfad4d832b2325d0198e76c616cc90af21f
sunhy <sunhy@corp.21cn.com>
2019-11-07 16:22:58 +0800

    add init

---
+++ src/main/java/com/sun/tomorrow/core/base/TreeNode.java
@@ -0,0 +1,11 @@
+package com.sun.tomorrow.core.base;
+
+/**
+ * @Author roger sun
+ * @Date 2019/11/7 16:22
+ */
+public class TreeNode {
+
+
+
+}
---
+++ src/main/java/com/sun/tomorrow/core/test.java
@@ -0,0 +1,8 @@
+package com.sun.tomorrow.core;
+
+/**
+ * @Author roger sun
+ * @Date 2019/11/7 16:21
+ */
+public class test {
+}
