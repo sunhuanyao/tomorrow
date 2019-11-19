54d6d23260531cfe01b8722ee36630d47a3a994e
sunhuanyao <460156671@qq.com>
2019-11-19 23:12:18 +0800

    新增分裂节点

--- src/main/java/com/sun/tomorrow/core/container/BTreeFactory.java
+++ src/main/java/com/sun/tomorrow/core/container/BTreeFactory.java
@@ -58,11 +58,11 @@ public abstract class BTreeFactory<T> extends TreeFactory<T> implements TreeFact
     }
 
     /**
-     * 该二分用作
-     * @param orgin
-     * @param valIndex
-     * @param key
-     * @return
+     * 该二分用作 寻找 插入节点应该插入的位置
+     * @param orgin  原数组
+     * @param valIndex  数组包含真实数大小
+     * @param key  插入值
+     * @return 位置
      */
     @SuppressWarnings("unchecked")
     public int midFind(Object[] orgin, int valIndex, T key){
@@ -84,6 +84,13 @@ public abstract class BTreeFactory<T> extends TreeFactory<T> implements TreeFact
         return r;
     }
 
+    /**
+     * 将 key 插入到origin[index]
+     * @param origin 即将插入的数组
+     * @param key    插入的数
+     * @param index  插入的位置
+     * @return  完成操作的数组
+     */
     public Object[] insertIntoArray(Object[] origin, T key, int index){
         if(origin.length > cap) throw new ArrayIndexOutOfBoundsException("beyond the cap: " + cap);
         for (int i = origin.length - 1; i > index; i --){
@@ -93,16 +100,56 @@ public abstract class BTreeFactory<T> extends TreeFactory<T> implements TreeFact
         return origin;
     }
 
+    /**
+     * 完成插入操作
+     * @param root
+     * @param key
+     * @return
+     */
     public BTreeNode<T> addIntoLevelNode(BTreeNode<T> root, T key){
         int index = midFind(root.getVals(), root.getNowValIndex(), key);
         root.setVals(insertIntoArray(root.getVals(), key, index));
         root.valIndexIncrease();
         return root;
     }
+    // 获取  分裂后，左节点的长度
+    private int getLeftSplitNodeLength(int tmp){
+        return (tmp + 1) / 2;
+    }
+
+
+    //获取 分裂后， 右节点的起始位置
+    private int getRightStartIndex(int tmp){
+        return getLeftSplitNodeLength(tmp) + 1;
+    }
 
     public BTreeNode<T> splitNode(BTreeNode<T> root){
+        BTreeNode<T> newRoot = new BTreeNode<>(cap);
+        Object[] left = new Object[cap];
+        Object[] right = new Object[cap];
+        System.arraycopy( root.getVals(),
+                0,
+                left,
+                0,
+                getLeftSplitNodeLength(root.getNowValIndex())
+        );
 
+        System.arraycopy( root.getVals(),
+                getRightStartIndex(root.getNowValIndex()),
+                right,
+                0,
+                root.getNowValIndex() - getLeftSplitNodeLength(root.getNowValIndex())
+        );
+        BTreeNode[] leftChild = new BTreeNode[cap + 1];
+        BTreeNode[] rightChild = new BTreeNode[cap + 1];
 
+        System.arraycopy(
+                root.getChilds(),
+                0,
+                leftChild,
+                0,
+                getLeftSplitNodeLength(root.getNowValIndex()) + 1
+        );
 
 
 
