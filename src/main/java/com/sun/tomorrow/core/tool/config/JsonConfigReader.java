c8dd1aa824d77ac2631f1d471e68b11575daa3e6
rogersunhy <460156671@qq.com>
2021-02-15 17:14:01 +0800

    【sun】修复bug，添加es实例

--- src/main/java/com/sun/tomorrow/core/tool/config/JsonConfigReader.java
+++ src/main/java/com/sun/tomorrow/core/tool/config/JsonConfigReader.java
@@ -1,26 +1,33 @@
 package com.sun.tomorrow.core.tool.config;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Type;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+/**
+ * 用法：example  获取 key 为redis的配置  这里的holder 需要与解析里面的json对应上。
+ * Holder info = JsonConfigReader.getInstance().getObject(Holder.class, "redis");
+ *
+ */
 @Slf4j
 public class JsonConfigReader {
 
-    private static final String DEFAULT_PATH = "config.txt";
+    private static final String DEFAULT_PATH = "config.json";
 
     private Map<String, Object> datas = new HashMap<>();
+
+    private String origin;
 
     public JsonConfigReader () {
         init();
@@ -30,23 +37,41 @@ public class JsonConfigReader {
         return Holder.instance;
     }
 
-    public static class Holder {
+    private static class Holder {
         public static final JsonConfigReader instance  = new JsonConfigReader();
     }
 
     public void init() {
-        try(InputStream inputStream = JsonConfigReader.class.getClassLoader().getResourceAsStream(DEFAULT_PATH)) {
-            if (inputStream == null) {
-                log.error("input is null.");
-                return ;
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        String content = parse(cl, DEFAULT_PATH);
+        if(content == null) {
+            log.error("error to init config txt");
+            return ;
+        }
+        this.origin = content;
+        this.datas = JSONObject.parseObject(content, new TypeReference<Map<String, Object>>(){}.getType());
+
+    }
+
+    public static String parse(ClassLoader classLoader, String fileName) {
+        try(InputStream inputStream = classLoader.getResourceAsStream(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String cnt;
+            StringBuilder sb = new StringBuilder();
+            while((cnt = bufferedReader.readLine()) != null) {
+                sb.append(cnt);
             }
-            this.datas = JSONObject.parseObject(inputStream, new TypeReference<Map<String, Object>>(){}.getType());
+            return sb.toString();
         } catch (IOException ex) {
             log.error("parse txt happen io exception:", ex);
         }
+        return null;
     }
 
     public <T> T getObject (Class<T> clazz, String key) {
+        if (StringUtils.isEmpty(key)) {
+            return JSONObject.parseObject(this.origin, clazz);
+        }
         Object object = this.datas.get(key);
         return JSONObject.parseObject(String.valueOf(object), clazz);
     }
---
+++ src/main/java/com/sun/tomorrow/core/tool/elasticsearch/EsInstance.java
@@ -0,0 +1,60 @@
+package com.sun.tomorrow.core.tool.elasticsearch;
+
+import com.sun.tomorrow.core.tool.config.JsonConfigReader;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.HttpPost;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+
+import java.io.IOException;
+
+@Getter
+public class EsInstance {
+
+    private RestHighLevelClient client;
+
+    private EsInstance() {
+        init();
+    }
+
+    public static EsInstance getInstance () {
+        return Holder.instance;
+    }
+
+    private void init() {
+        EsEntity esEntity = JsonConfigReader.getInstance().getObject(EsEntity.class, "elasticsearch");
+        this.client =  new RestHighLevelClient(RestClient.builder(new HttpHost(esEntity.host, esEntity.port, esEntity.ssl)));
+    }
+
+    private static class Holder {
+        public static final EsInstance instance = new EsInstance();
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class EsEntity {
+        private String host;
+        private int port;
+        private String ssl;
+    }
+
+    public static boolean isIndexExists(RestHighLevelClient client, String indexName) throws IOException {
+        GetIndexRequest indexRequest = new GetIndexRequest(indexName);
+        return client.indices().exists(indexRequest, RequestOptions.DEFAULT);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getInstance().getClient().indices().exists(new GetIndexRequest("test_1"), RequestOptions.DEFAULT));
+    }
+
+
+}
